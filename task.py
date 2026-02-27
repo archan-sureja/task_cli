a393578cd9d41de9266490a72d6ee4fefe48d7af
archan-sureja <archan.sureja@simformsolutions.com>
2026-02-27 16:33:17 +0530

    feature/task-class created initial strcuture

---
+++ task.py
@@ -0,0 +1,11 @@
+import uuid 
+from datetime import datetime 
+class Task:
+    def __init__(self,description):
+        self.id = uuid.uuid4()[:5] 
+        self.description = description
+        self.status = 'todo'
+        self.createdAt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.updatedAt = None
+    
+    
